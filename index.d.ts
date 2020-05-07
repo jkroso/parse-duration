a4c9dc372e8b9c704849a41c0fa8095c14e9d886
Evgeny Kruglov <ekruglov@gmail.com>
2020-05-07 23:08:17 +0200

    style: improve typings

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,16 @@
 /**
  * convert `str` to ms
  */
-export default function parse (str: string): number | null
+type Units =
+  'nanosecond' | 'ns' |
+  'µs' | 'μs' | 'us' | 'microsecond' |
+  'millisecond' | 'ms' |
+  'second' | 'sec' | 's' |
+  'minute' | 'min' | 'm' |
+  'hour' | 'hr' | 'h' |
+  'day' | 'd' |
+  'week' | 'wk' | 'w' |
+  'month' | 'b' |
+  'year' | 'yr' | 'y'
+
+export default function parse (str: string, format?: Units): number | null
