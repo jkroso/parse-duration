8ee316c69809946e6dda81293251fe176a9f25be
Dmitry Iv <df.creative@gmail.com>
2024-12-01 12:55:35 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -5,44 +5,44 @@ let durationRE = /(-?(?:\d+\.?\d*|\d*\.?\d+)(?:e[-+]?\d+)?)\s*([\p{L}]*)/uig
  * conversion ratios
  */
 
-parse.nanosecond =
-parse.ns = 1 / 1e6
-
-parse['µs'] =
-parse['μs'] =
-parse.us =
-parse.microsecond = 1 / 1e3
-
-parse.millisecond =
-parse.ms =
-parse[''] = 1
-
-parse.second =
-parse.sec =
-parse.s = parse.ms * 1000
-
-parse.minute =
-parse.min =
-parse.m = parse.s * 60
-
-parse.hour =
-parse.hr =
-parse.h = parse.m * 60
-
-parse.day =
-parse.d = parse.h * 24
-
-parse.week =
-parse.wk =
-parse.w = parse.d * 7
+parse.year =
+  parse.yr =
+  parse.y = 60000 * 60 * 24 * 365.25
 
 parse.month =
-parse.b =
-parse.d * (365.25 / 12)
+  parse.b = 60000 * 60 * 24 * (365.25 / 12)
 
-parse.year =
-parse.yr =
-parse.y = parse.d * 365.25
+parse.week =
+  parse.wk =
+  parse.w = 60000 * 60 * 24 * 7
+
+parse.day =
+  parse.d = 60000 * 60 * 24
+
+parse.hour =
+  parse.hr =
+  parse.h = 60000 * 60
+
+parse.minute =
+  parse.min =
+  parse.m = 60000
+
+parse.second =
+  parse.sec =
+  parse.s = 1000
+
+parse.millisecond =
+  parse.millisec =
+  parse.ms = 1
+
+parse['µs'] =
+  parse['μs'] =
+  parse.us =
+  parse.microsecond = 1 / 1e3
+
+parse.nanosecond =
+  parse.ns = 1 / 1e6
+
 
 /**
  * convert `str` to ms
@@ -52,21 +52,23 @@ parse.y = parse.d * 365.25
  * @return {Number}
  */
 
-function parse(str='', format='ms'){
-  var result = null
+function parse(str = '', format = 'ms') {
+  var result = null, prevUnits
   // ignore commas/placeholders
-  str = (str+'').replace(/(\d)[,_](\d)/g, '$1$2')
-  var isNegative = str[0] === '-';
-  str.replace(durationRE, function(_, n, units){
-    units = unitRatio(units)
-    if (units) result = (result || 0) + Math.abs(parseFloat(n, 10)) * units
+  str = (str + '').replace(/(\d)[,_](\d)/g, '$1$2')
+  str.replace(durationRE, function (_, n, units) {
+    // if no units, find next smallest units or fall back to format value (ms)
+    if (!units) {
+      if (prevUnits) {
+        for (var u in parse) if (parse[u] < prevUnits) { units = u; break }
+      }
+      else units = format
+    }
+    units = parse[units] || parse[units.toLowerCase().replace(/s$/, '')]
+    if (units) result = (result || 0) + Math.abs(parseFloat(n, 10)) * units, prevUnits = units
   })
 
-  return result && ((result / (unitRatio(format) || 1)) * (isNegative ? -1 : 1))
-}
-
-function unitRatio(str) {
-  return parse[str] || parse[str.toLowerCase().replace(/s$/, '')]
+  return result && ((result / (parse[format] || 1)) * (str[0] === '-' ? -1 : 1))
 }
 
 export default parse
