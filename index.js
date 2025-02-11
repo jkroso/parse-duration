1d8086e599cc3c0c024e492bf595bd00bb4fadf3
Dmitry Iv. <df.creative@gmail.com>
2025-02-10 19:00:30 -0500

    Minot perf optimization attempt

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 import en from './locale/en.js'
 
-const durationRE = /(-?(?:\d{1,16}(?:\.\d{1,16})?|\.\d{1,16})(?:[eE][-+]?\d{1,4})?)\s?([\p{L}]{0,14})/gu
+const durationRE = /((?:\d{1,16}(?:\.\d{1,16})?|\.\d{1,16})(?:[eE][-+]?\d{1,4})?)\s?([\p{L}]{0,14})/gu
 
 parse.unit = en
 
 /**
  * convert `str` to ms
  *
- * @param {String} str
- * @param {String} format
- * @return {Number}
+ * @param {string} str
+ * @param {string} format
+ * @return {number}
  */
 export default function parse(str = '', format = 'ms') {
   let result = null, prevUnits
 
   String(str)
-    .replace(new RegExp(`(?<=\\d)[${parse.unit.placeholder}${parse.unit.group}](?=\\d)`, 'g'), '')  // clean up group separators / placeholders
+    .replace(new RegExp(`(\\d)[${parse.unit.placeholder}${parse.unit.group}](\\d)`, 'g'), '$1$2')  // clean up group separators / placeholders
     .replace(parse.unit.decimal, '.') // normalize decimal separator
     .replace(durationRE, (_, n, units) => {
     // if no units, find next smallest units or fall back to format value
     // eg. 1h30 -> 1h30m
     if (!units) {
       if (prevUnits) {
-        for (var u in parse.unit) if (parse.unit[u] < prevUnits) { units = u; break }
+        for (const u in parse.unit) if (parse.unit[u] < prevUnits) { units = u; break }
       }
       else units = format
     }
     else units = units.toLowerCase()
 
-    units = parse.unit[units] || parse.unit[units.replace(/s$/, '')]
+    prevUnits = units = parse.unit[units] || parse.unit[units.replace(/s$/, '')]
 
-    if (units) result = (result || 0) + Math.abs(parseFloat(n, 10)) * units, prevUnits = units
+    if (units) result = (result || 0) + n * units
   })
 
   return result && ((result / (parse.unit[format] || 1)) * (str[0] === '-' ? -1 : 1))
